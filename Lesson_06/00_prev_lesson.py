bffa44909b85e938df7ac1ef3d85cf852f6ecc73
antohaUa <antoha.ua@gmail.com>
2023-12-18 21:29:21 +0200

    Lesson_06

---
+++ Lesson_06/00_prev_lesson.py
@@ -0,0 +1,13 @@
+# next lessons in 2023 end - start 2024
+# Agreement for such dates: 21-Dec, 28-Dec, 04-01-2024
+
+# dicts/functions/cycles
+# hardcodes
+
+l1 = (1, 2, 3, 4, 5)
+
+v1, *v_middle, v_last = l1
+print(v1)
+print(v_last)
+print(v_middle)
+print(type(v_middle))
