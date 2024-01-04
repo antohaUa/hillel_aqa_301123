74552e9cac61d3ab9befef6240555f7c07dc0968
antohaUa <antoha.ua@gmail.com>
2024-01-04 22:43:27 +0200

    Hometask_09

---
+++ Lesson_09/Hometask/09_if_elif_else.py
@@ -0,0 +1,6 @@
+# given values w,x,y,z
+# write if-elif-else statement that will search minimum value and print smth aka "'y' is minimum value'
+# where 'y' is variable name (not value)
+# advice use python debugger and different values to check your algorithm
+# optionally you can check your algorithm somehow with assert statements
+w, x, y, z = 100, 200, 40, 300
