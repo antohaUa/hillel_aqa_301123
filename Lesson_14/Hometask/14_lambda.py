223586c64a489efa4bbc934f90cb8ed7624b7fa4
antohaUa <antoha.ua@gmail.com>
2024-01-22 21:28:10 +0200

    Hometask_14

---
+++ Lesson_14/Hometask/14_lambda.py
@@ -0,0 +1,7 @@
+# Given: list of integers/floats with lambda function as one of the element inside list
+# Write function that will produce new list by applying lambda to all integers and floats
+# Input: [lambda a: a + 2, 9, 3, 1, 0]
+# Output: [11, 5, 3, 2]
+
+# Input: [9, 2, 3, lambda a: a / 2.0, 1, 0]
+# Output: [4.5, 1, 1.5, 0.5, 0.0]
